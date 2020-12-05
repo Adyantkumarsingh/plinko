5d1e63e6452b9d3a328f7c72dd7a47b21defa9a8
Adyantkumarsingh <70712174+Adyantkumarsingh@users.noreply.github.com>
2020-12-05 15:05:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var particles = [];
+var plinkos = [];
+var particles = [];
+var ground
+
+
+function setup() {
+  createCanvas(480,800);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
